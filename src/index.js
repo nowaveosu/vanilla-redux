b902dac7caa83ad1c0bf10eff69a6f2297c21e3d
nowaveosu <nowaveosu@gmail.com>
2024-12-18 23:46:09 +0900

    [Add] console.log 제거

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
 const countModifier = (count = 0) => {
-	console.log(count);
 	return "count";
 };
 const countStore = createStore(countModifier);
-console.log(countStore.getState());
