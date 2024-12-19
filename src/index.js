def28880f69ec201eac7805020a1174f8446d0b2
nowaveosu <nowaveosu@gmail.com>
2024-12-19 21:15:33 +0900

    [Add] add, minus 기능 추가

--- src/index.js
+++ src/index.js
@@ -4,8 +4,23 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+//reducer
 const countModifier = (count = 0, action) => {
-	console.log(count, action);
-	return "count";
+	if (action.type === "ADD") {
+		return count + 1;
+	} else if (action.type === "MINUS") {
+		return count - 1;
+	} else {
+		return count;
+	}
 };
+
+//1. store 만들기, reducer 넣어줘야함
 const countStore = createStore(countModifier);
+
+//action에 부르기
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
+
+console.log(countStore.getState());
