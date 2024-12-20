51bc5baf7bab3c7cad62d63f792d49239a632728
nowaveosu <nowaveosu@gmail.com>
2024-12-20 19:34:38 +0900

    [Add] todo 값 추가

--- src/index.js
+++ src/index.js
@@ -7,10 +7,12 @@ const ul = document.querySelector("ul");
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
+//state를 절대 변형하지 말고 값이 추가된 새로운 state를 리턴할것
+
 const reducer = (state = [], action) => {
 	switch (action.type) {
 		case ADD_TODO:
-			return [];
+			return [...state, { text: action.text }];
 		case DELETE_TODO:
 			return [];
 		default:
@@ -19,6 +21,8 @@ const reducer = (state = [], action) => {
 };
 
 const store = createStore(reducer);
+
+store.subscribe(() => console.log(store.getState()));
 
 const onSubmit = (e) => {
 	e.preventDefault();
