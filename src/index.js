fce3445f1fbec26b639ee5f071b087d554a2f319
nowaveosu <nowaveosu@gmail.com>
2024-12-28 21:56:30 +0900

    [Add] deleteToDo 구현

--- src/index.js
+++ src/index.js
@@ -24,22 +24,31 @@ const store = createStore(reducer);
 
 store.subscribe(() => console.log(store.getState()));
 
+const addToDo = (text) => {
+	store.dispatch({ type: ADD_TODO, text });
+};
+
+const deleteToDo = (e) => {
+	const id = e.target.parentNode.id;
+	store.dispatch({ type: DELETE_TODO, id });
+};
+
 const paintToDos = () => {
 	const toDos = store.getState();
 	ul.innerHTML = "";
 	toDos.forEach((toDo) => {
 		const li = document.createElement("li");
+		const btn = document.createElement("button");
+		btn.innerText = "DEL";
+		btn.addEventListener("click", deleteToDo);
 		li.id = toDo.id;
 		li.innerText = toDo.text;
+		li.appendChild(btn);
 		ul.appendChild(li);
 	});
 };
 
 store.subscribe(paintToDos);
-
-const addToDo = (text) => {
-	store.dispatch({ type: ADD_TODO, text });
-};
 
 const onSubmit = (e) => {
 	e.preventDefault();
