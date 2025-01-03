148edcec6e3205c51ffb01a1d0f4ef1c985f0a6d
nowaveosu <nowaveosu@gmail.com>
2025-01-03 20:41:37 +0900

    [Add] onSubmit 구현

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,1 +1,25 @@
-export default () => "Home";
+import React from "react";
+import { useState } from "react";
+
+function Home() {
+	const [text, setText] = useState("");
+	function onChange(e) {
+		setText(e.target.value);
+	}
+	function onSubmit(e) {
+		e.preventDefault();
+		console.log(text);
+	}
+	return (
+		<>
+			<h1>To Do</h1>
+			<form onSubmit={onSubmit}>
+				<input type="text" value={text} onChange={onChange} />
+				<button>Add</button>
+			</form>
+			<ul></ul>
+		</>
+	);
+}
+
+export default Home;
