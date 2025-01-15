f18dc5c65d520204624a2cb281b60a381a8b4165
nowaveosu <nowaveosu@gmail.com>
2025-01-15 18:59:52 +0900

    [Add] createReducer 사용

--- src/store.js
+++ src/store.js
@@ -1,25 +1,36 @@
 import { createStore } from "redux";
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 const ADD = "ADD";
 const DELETE = "DELETE";
 
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
-console.log(addToDo.type, deleteToDo());
-const reducer = (state = [], action) => {
-	switch (action.type) {
-		case addToDo.type:
-			console.log(action);
-			return [
-				{ text: action.payload, id: Date.now() },
-				...state,
-			];
-		case deleteToDo.type:
+
+// const reducer = (state = [], action) => {
+// 	switch (action.type) {
+// 		case addToDo.type:
+// 			console.log(action);
+// 			return [
+// 				{ text: action.payload, id: Date.now() },
+// 				...state,
+// 			];
+// 		case deleteToDo.type:
+// 			return state.filter((toDo) => toDo.id !== action.payload);
+// 		default:
+// 			return state;
+// 	}
+// };
+
+// state를 mutate하거나, 새로운 state 리턴하거나 할수있음
+const reducer = createReducer([], (builder) => {
+	builder
+		.addCase(addToDo, (state, action) => {
+			state.push({ id: Date.now(), text: action.payload });
+		})
+		.addCase(deleteToDo, (state, action) => {
 			return state.filter((toDo) => toDo.id !== action.payload);
-		default:
-			return state;
-	}
-};
+		});
+});
 
 const store = createStore(reducer);
 
